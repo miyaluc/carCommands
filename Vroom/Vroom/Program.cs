7360640ca1bf4c9a25d481c831b2466779085887
miyaluc <srlucas3@gmail.com>
2017-12-02 14:58:31 -0800

    added menu navigation for your new Tesla

---
+++ Vroom/Vroom/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Vroom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Car Spyder = new Car();
+            Spyder.NavigateMenu();
+
+            Console.ReadLine();
+        }
+    }
+}
